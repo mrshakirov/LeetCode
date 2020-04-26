eb4b1259edc272a100e018afd78c97007488ed23
Nail Shakirov <mrshakirov@gmail.com>
2020-04-26 13:47:54 +0300

    solved Min Stack task

---
+++ 30-Day LeetCoding Challenge/Week 2/Min Stack.py
@@ -0,0 +1,37 @@
+#https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3292/
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = []
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min: 
+            self.min.append(x)
+        else:
+            self.min.append(min(self.min[-1], x)) 
+        
+
+    def pop(self) -> None:
+        self.min.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
